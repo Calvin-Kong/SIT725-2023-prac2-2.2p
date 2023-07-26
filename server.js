6c51a167d8dbf828e11d8b6e367c5889e598dad9
Calvin Kong <84771381+Calvin-Kong@users.noreply.github.com>
2023-07-26 22:24:43 +1000

    Fixed Result not showing in server end/URl
    - Result display in URL and webpage
    - Upgrade code

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 let port = 3000;
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + "/"));
 
-app.get('/', (req, res) => {
-    res.json('index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// fetch input from index.html
+app.get("/multiplyResult", (req, res) => {
+  let result = req.query.result;
+
+  console.log("Result is", result);
+  res.send(result);
+  // print out result in server page
+});
+
+app.get("/divideResult", (req, res) => {
+  let result = req.query.result;
+
+  console.log("Result is", result);
+  res.send(result);
+  // print out result in server page
+});
+
+app.get("/sumResult", (req, res) => {
+  let result = req.query.result;
+
+  console.log("Result is", result);
+  res.send(result);
+  // print out result in server page
+});
+
+app.get("/subtractResult", (req, res) => {
+  let result = req.query.result;
+  res.send(result);
+  // print out result in server page
 });
 
 app.listen(port, () => {
-    console.log("Server running, listenting to port: " + port);
+  console.log("Server running, listenting to port: " + port);
 });
